85575e2c03351d75aa9128de7b32415651183242
legobuilder0813 <legobuilder0813@gmail.com>
2014-12-16 21:26:20 -0500

    Add warp/disposal signs.

--- src/main/java/us/mcmagic/magicassistant/listeners/PlayerInteract.java
+++ src/main/java/us/mcmagic/magicassistant/listeners/PlayerInteract.java
@@ -1,6 +1,9 @@
 package us.mcmagic.magicassistant.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,9 +14,11 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 import us.mcmagic.magicassistant.MagicAssistant;
 import us.mcmagic.magicassistant.PlayerData;
+import us.mcmagic.magicassistant.Warp;
 import us.mcmagic.magicassistant.utils.BandUtil;
 import us.mcmagic.magicassistant.utils.InventoryType;
 import us.mcmagic.magicassistant.utils.InventoryUtil;
+import us.mcmagic.magicassistant.utils.WarpUtil;
 
 import java.util.Arrays;
 
@@ -29,6 +34,27 @@ public class PlayerInteract implements Listener {
         Player player = event.getPlayer();
         PlayerData data = MagicAssistant.getPlayerData(player.getUniqueId());
         if (event.getAction().equals(Action.PHYSICAL)) {
+            return;
+        }
+        Material type = event.getClickedBlock().getType();
+        if (type.equals(Material.SIGN) || type.equals(Material.SIGN_POST) || type.equals(Material.WALL_SIGN)) {
+            Sign s = (Sign) event.getClickedBlock().getState();
+            if (s.getLine(0).equals("[" + ChatColor.BLUE + "Disposal" + ChatColor.RESET + "]")) {
+                player.openInventory(Bukkit.createInventory(player, 54, ChatColor.BLUE + "Disposal"));
+                return;
+            }
+            if (s.getLine(0).equals("[" + ChatColor.BLUE + "Warp" + ChatColor.RESET + "]")) {
+                Warp warp = WarpUtil.findWarp(ChatColor.stripColor(s.getLine(1)));
+                if (warp == null) {
+                    player.sendMessage(ChatColor.RED + "That warp does not exist, sorry!");
+                    return;
+                }
+                player.teleport(warp.getLocation());
+                player.sendMessage(ChatColor.BLUE + "You have arrived at "
+                        + ChatColor.WHITE + "[" + ChatColor.GREEN + warp.getName()
+                        + ChatColor.WHITE + "]");
+                return;
+            }
             return;
         }
         PlayerInventory pi = player.getInventory();
