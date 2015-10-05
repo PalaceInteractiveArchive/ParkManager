e2157af7bcc03ec65748ca108e363859535913cd
legobuilder0813 <legobuilder0813@gmail.com>
2015-10-04 20:43:57 -0400

    Remove /ptime message for target player

--- src/main/java/us/mcmagic/magicassistant/commands/Commandptime.java
+++ src/main/java/us/mcmagic/magicassistant/commands/Commandptime.java
@@ -22,10 +22,6 @@ public class Commandptime implements CommandExecutor {
                 switch (args[0]) {
                     case "day":
                         tp.setPlayerTime(1000, false);
-                        tp.sendMessage(ChatColor.GREEN
-                                + "Your time has been set to "
-                                + ChatColor.DARK_AQUA + tp.getPlayerTime()
-                                + ChatColor.GREEN + "!");
                         sender.sendMessage(ChatColor.DARK_AQUA + tp.getName()
                                 + "'s " + ChatColor.GREEN + "time has been set to "
                                 + ChatColor.DARK_AQUA + "1000" + ChatColor.GREEN
@@ -33,10 +29,6 @@ public class Commandptime implements CommandExecutor {
                         break;
                     case "noon":
                         tp.setPlayerTime(6000, false);
-                        tp.sendMessage(ChatColor.GREEN
-                                + "Your time has been set to "
-                                + ChatColor.DARK_AQUA + tp.getPlayerTime()
-                                + ChatColor.GREEN + "!");
                         sender.sendMessage(ChatColor.DARK_AQUA + tp.getName()
                                 + "'s " + ChatColor.GREEN + "time has been set to "
                                 + ChatColor.DARK_AQUA + "6000" + ChatColor.GREEN
@@ -44,10 +36,6 @@ public class Commandptime implements CommandExecutor {
                         break;
                     case "night":
                         tp.setPlayerTime(16000, false);
-                        tp.sendMessage(ChatColor.GREEN
-                                + "Your time has been set to "
-                                + ChatColor.DARK_AQUA + tp.getPlayerTime()
-                                + ChatColor.GREEN + "!");
                         sender.sendMessage(ChatColor.DARK_AQUA + tp.getName()
                                 + "'s " + ChatColor.GREEN + "time has been set to "
                                 + ChatColor.DARK_AQUA + "16000" + ChatColor.GREEN
@@ -55,8 +43,6 @@ public class Commandptime implements CommandExecutor {
                         break;
                     case "reset":
                         tp.resetPlayerTime();
-                        tp.sendMessage(ChatColor.GREEN
-                                + "Your time now matches the server.");
                         sender.sendMessage(ChatColor.DARK_AQUA + tp.getName()
                                 + "'s " + ChatColor.GREEN
                                 + "time now matches the server.");
@@ -65,10 +51,6 @@ public class Commandptime implements CommandExecutor {
                         if (isInt(args[0])) {
                             int time = Integer.parseInt(args[0]);
                             tp.setPlayerTime(time, false);
-                            tp.sendMessage(ChatColor.GREEN
-                                    + "Your time has been set to "
-                                    + ChatColor.DARK_AQUA + time
-                                    + ChatColor.GREEN + "!");
                             sender.sendMessage(ChatColor.DARK_AQUA + tp.getName()
                                     + "'s " + ChatColor.GREEN
                                     + "time has been set to " + ChatColor.DARK_AQUA
@@ -137,45 +119,30 @@ public class Commandptime implements CommandExecutor {
             switch (args[0]) {
                 case "day":
                     tp.setPlayerTime(1000, false);
-                    tp.sendMessage(ChatColor.GREEN + "Your time has been set to "
-                            + ChatColor.DARK_AQUA + tp.getPlayerTime()
-                            + ChatColor.GREEN + "!");
                     player.sendMessage(ChatColor.DARK_AQUA + tp.getName() + "'s "
                             + ChatColor.GREEN + "time has been set to "
                             + ChatColor.DARK_AQUA + "1000" + ChatColor.GREEN + "!");
                     break;
                 case "noon":
                     tp.setPlayerTime(6000, false);
-                    tp.sendMessage(ChatColor.GREEN + "Your time has been set to "
-                            + ChatColor.DARK_AQUA + tp.getPlayerTime()
-                            + ChatColor.GREEN + "!");
                     player.sendMessage(ChatColor.DARK_AQUA + tp.getName() + "'s "
                             + ChatColor.GREEN + "time has been set to "
                             + ChatColor.DARK_AQUA + "6000" + ChatColor.GREEN + "!");
                     break;
                 case "night":
                     tp.setPlayerTime(16000, false);
-                    tp.sendMessage(ChatColor.GREEN + "Your time has been set to "
-                            + ChatColor.DARK_AQUA + tp.getPlayerTime()
-                            + ChatColor.GREEN + "!");
                     player.sendMessage(ChatColor.DARK_AQUA + tp.getName() + "'s "
                             + ChatColor.GREEN + "time has been set to "
                             + ChatColor.DARK_AQUA + "16000" + ChatColor.GREEN + "!");
                     break;
                 case "reset":
                     tp.resetPlayerTime();
-                    tp.sendMessage(ChatColor.GREEN
-                            + "Your time now matches the server.");
                     player.sendMessage(ChatColor.DARK_AQUA + tp.getName() + "'s "
                             + ChatColor.GREEN + "time now matches the server.");
                     break;
                 default:
                     if (isInt(args[0])) {
                         tp.setPlayerTime(Integer.parseInt(args[0]), false);
-                        tp.sendMessage(ChatColor.GREEN
-                                + "Your time has been set to "
-                                + ChatColor.DARK_AQUA + tp.getPlayerTime()
-                                + ChatColor.GREEN + "!");
                         player.sendMessage(ChatColor.DARK_AQUA + tp.getName()
                                 + "'s " + ChatColor.GREEN + "time has been set to "
                                 + ChatColor.DARK_AQUA + Integer.parseInt(args[0])
