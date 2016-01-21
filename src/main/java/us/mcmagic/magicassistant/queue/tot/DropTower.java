799383c5dda3db91c7779f78d57a552a4d4a983b
legobuilder0813 <legobuilder0813@gmail.com>
2016-01-21 15:02:06 -0500

    Start work on TOT stuffs

---
+++ src/main/java/us/mcmagic/magicassistant/queue/tot/DropTower.java
@@ -0,0 +1,29 @@
+package us.mcmagic.magicassistant.queue.tot;
+
+import java.util.Random;
+
+/**
+ * Created by Marc on 12/30/15
+ */
+public enum DropTower {
+    ECHO, FOXTROT;
+
+    private TowerLayout layout;
+
+    DropTower() {
+        randomizeLayout();
+    }
+
+    public TowerLayout getLayout() {
+        return layout;
+    }
+
+    public TowerLayout randomizeLayout() {
+        return layout = new TowerLayout(getRandomBetween(89, 103), getRandomBetween(69, 85));
+    }
+
+    private int getRandomBetween(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
+}
---
+++ src/main/java/us/mcmagic/magicassistant/queue/tot/TowerPreShow.java
@@ -0,0 +1,8 @@
+package us.mcmagic.magicassistant.queue.tot;
+
+/**
+ * Created by Marc on 12/30/15
+ */
+public enum TowerPreShow {
+    ECHO, FOXTROT
+}
