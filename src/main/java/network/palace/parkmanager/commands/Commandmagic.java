440a34577ecb04f9f1ab5a15a74a685f827f0e73
Legobuilder0813 <legobuilder0813@gmail.com>
2017-06-27 00:49:17 -0400

    Finally fixed /magic effect particle

--- src/main/java/network/palace/parkmanager/commands/Commandmagic.java
+++ src/main/java/network/palace/parkmanager/commands/Commandmagic.java
@@ -72,7 +72,7 @@ public class Commandmagic extends CoreCommand {
                 }
                 if (args[1].equalsIgnoreCase("particle")) {
                     if (args.length == 11) {
-                        Particle effect = Particle.valueOf(args[2]);
+                        Particle effect = getParticle(args[2]);
                         Location loc = locFromArray(args[3], args[4], args[5]);
                         try {
                             double offsetX = Float.parseFloat(args[6]);
@@ -1037,6 +1037,88 @@ public class Commandmagic extends CoreCommand {
             }
         }
         return list;
+    }
+
+    private Particle getParticle(String s) {
+        switch (s.toLowerCase()) {
+            case "barrier":
+                return Particle.BARRIER;
+            case "bubble":
+                return Particle.WATER_BUBBLE;
+            case "cloud":
+                return Particle.CLOUD;
+            case "crit":
+                return Particle.CRIT;
+            case "depthsuspend":
+                return Particle.SUSPENDED_DEPTH;
+            case "dragonbreath":
+                return Particle.DRAGON_BREATH;
+            case "driplava":
+                return Particle.DRIP_LAVA;
+            case "dripwater":
+                return Particle.DRIP_WATER;
+            case "enchantmenttable":
+                return Particle.ENCHANTMENT_TABLE;
+            case "explode":
+                return Particle.EXPLOSION_NORMAL;
+            case "fireworksspark":
+                return Particle.FIREWORKS_SPARK;
+            case "flame":
+                return Particle.FLAME;
+            case "footstep":
+                return Particle.FOOTSTEP;
+            case "happyvillager":
+                return Particle.VILLAGER_HAPPY;
+            case "heart":
+                return Particle.HEART;
+            case "hugeexplosion":
+                return Particle.EXPLOSION_HUGE;
+            case "instantspell":
+                return Particle.SPELL_INSTANT;
+            case "largeexplode":
+                return Particle.EXPLOSION_LARGE;
+            case "largesmoke":
+                return Particle.SMOKE_LARGE;
+            case "lava":
+                return Particle.LAVA;
+            case "magiccrit":
+                return Particle.CRIT_MAGIC;
+            case "mobspell":
+                return Particle.SPELL_MOB;
+            case "mobspellambient":
+                return Particle.SPELL_MOB_AMBIENT;
+            case "note":
+                return Particle.NOTE;
+            case "portal":
+                return Particle.PORTAL;
+            case "reddust":
+                return Particle.REDSTONE;
+            case "slime":
+                return Particle.SLIME;
+            case "smoke":
+                return Particle.SMOKE_NORMAL;
+            case "snowballpoof":
+                return Particle.SNOWBALL;
+            case "snowshovel":
+                return Particle.SNOW_SHOVEL;
+            case "spell":
+                return Particle.SPELL;
+            case "spit":
+                return Particle.SPIT;
+            case "splash":
+                return Particle.WATER_SPLASH;
+            case "suspend":
+                return Particle.SUSPENDED;
+            case "totem":
+                return Particle.TOTEM;
+            case "townaura":
+                return Particle.TOWN_AURA;
+            case "wake":
+                return Particle.WATER_WAKE;
+            case "witchmagic":
+                return Particle.SPELL_WITCH;
+        }
+        return Particle.valueOf(s);
     }
 
     private static boolean isInt(String s) {
