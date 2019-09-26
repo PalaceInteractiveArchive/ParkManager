c4af5d96a6255aa517212ba54e5a2eb746f30af1
Marc <legobuilder0813@gmail.com>
2019-09-26 19:44:39 -0400

    Add ability to exit queue builder

--- src/main/java/network/palace/parkmanager/commands/queue/CreateCommand.java
+++ src/main/java/network/palace/parkmanager/commands/queue/CreateCommand.java
@@ -16,13 +16,18 @@ public class CreateCommand extends CoreCommand {
 
     @Override
     protected void handleCommand(CPlayer player, String[] args) throws CommandException {
+        if (args.length == 1 && args[0].equalsIgnoreCase("exit")) {
+            player.sendMessage(ChatColor.RED + "Exited the queue builder!");
+            player.getRegistry().removeEntry("queueBuilder");
+            return;
+        }
         if (player.getRegistry().hasEntry("queueBuilder")) {
             ((QueueBuilder) player.getRegistry().getEntry("queueBuilder")).nextStep(player, args);
             return;
         }
         QueueBuilder queue = new QueueBuilder();
         player.getRegistry().addEntry("queueBuilder", queue);
-        player.sendMessage(ChatColor.GREEN + "You've started creating a new queue! We're going to go step-by-step through this.");
+        player.sendMessage(ChatColor.GREEN + "You've started creating a new queue! We're going to go step-by-step through this. (Exit at any time with /queue exit)");
         player.sendMessage(ChatColor.GREEN + "First, let's give your queue a name. Run " + ChatColor.YELLOW + "/queue create [name]");
         player.sendMessage(ChatColor.DARK_AQUA + "" + ChatColor.ITALIC + "This name supports color codes! For example, '&aExample &dQueue' becomes '"
                 + ChatColor.GREEN + "Example " + ChatColor.LIGHT_PURPLE + "Queue" + ChatColor.DARK_AQUA + "'.");
